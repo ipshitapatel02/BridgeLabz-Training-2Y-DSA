b55cf6e3041f5eab45aee5614f2b90aa2eddaf84
Ipshita <ippu0206@gmail.com>
2026-04-09 21:20:40 +0530

    Max sliding window

---
+++ MaxSlidingWindow.java
@@ -0,0 +1,41 @@
+package stacks_and_queues;
+
+import java.util.*;
+
+public class MaxSlidingWindow {
+    public static int[] maxSlidingWindow(int[] nums, int k) {
+        int n = nums.length;
+        int[] result = new int[n - k + 1];
+        Deque<Integer> dq = new ArrayDeque<>();
+        for (int i = 0; i < n; i++) {
+            if (!dq.isEmpty() && dq.peekFirst() == i - k) {
+                dq.pollFirst();
+            }
+            while (!dq.isEmpty() && nums[dq.peekLast()] < nums[i]) {
+                dq.pollLast();
+            }
+            dq.offerLast(i);
+            if (i >= k - 1) {
+                result[i - k + 1] = nums[dq.peekFirst()];
+            }
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter no. of elements: ");
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+        System.out.print("Enter window size k: ");
+        int k = sc.nextInt();
+        int[] result = maxSlidingWindow(nums, k);
+        for (int x : result) {
+            System.out.print(x + " ");
+        }
+        sc.close();
+    }
+}
