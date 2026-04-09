81120d3e853d21e78424fb0a5e0c7a7eea306644
Ipshita <ippu0206@gmail.com>
2026-04-09 22:21:25 +0530

    Longest consecutive sequence

---
+++ ConsSeq.java
@@ -0,0 +1,38 @@
+package hashmaps_and_hashfunctions;
+
+import java.util.*;
+
+public class ConsSeq {
+    public static int longCons(int[] arr) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : arr) {
+            set.add(num);
+        }
+        int longest = 0;
+        for (int num : set) {
+            if (!set.contains(num - 1)) {
+                int currentNum = num;
+                int count = 1;
+                while (set.contains(currentNum + 1)) {
+                    currentNum++;
+                    count++;
+                }
+                longest = Math.max(longest, count);
+            }
+        }
+        return longest;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter no. of elements: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int result = longCons(arr);
+        System.out.println(result);
+        sc.close();
+    }
+}
